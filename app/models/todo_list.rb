079198202917716a261755b2950dbdb52e1202d0
midorineko <steven.inouye91@gmail.com>
2016-04-18 11:33:37 -0700

    added model support for lists

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,12 @@
 class TodoList < ActiveRecord::Base
 	has_many :todo_items
 	belongs_to :day
+
+	def has_todo_items
+		if todo_items.length === 0
+			return false
+		else
+			return true
+		end
+	end
 end
