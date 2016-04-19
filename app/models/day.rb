e7d8e121f9fb6a2f38b8dc03664fb77d1fab2dc8
midorineko <steven.inouye91@gmail.com>
2016-04-18 18:45:40 -0700

    added calendar color changing based on fill

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,4 +1,12 @@
 class Day < ActiveRecord::Base
 	has_many :todo_lists
 	has_many :todo_items, through: :todo_lists
+
+	def has_todo_lists
+		if todo_lists.length === 0
+			return false
+		else
+			return true
+		end
+	end
 end
